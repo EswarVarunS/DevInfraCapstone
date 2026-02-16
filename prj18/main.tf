c9bf830b5b4ad851305e7a096762d0d66f2ac981
eswar <s.eswarvarun@gmail.com>
2026-02-16 12:28:24 +0530

    gitops

--- prj18/main.tf
+++ prj18/main.tf
@@ -1,9 +1,1 @@
-terraform {
-  required_version = ">= 1.0"
-}
-
-resource "null_resource" "gitops_demo" {
-  provisioner "local-exec" {
-    command = "echo GitOps deployment from prj18 successful"
-  }
-}
+#gitops test
