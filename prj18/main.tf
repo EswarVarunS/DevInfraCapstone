37bf0870d825270b50de3c1f16bda7af4d0f03ac
eswar <s.eswarvarun@gmail.com>
2026-02-16 11:57:30 +0530

    GitOps setup using prj18

---
+++ prj18/main.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_version = ">= 1.0"
+}
+
+resource "null_resource" "gitops_demo" {
+  provisioner "local-exec" {
+    command = "echo GitOps deployment from prj18 successful"
+  }
+}
